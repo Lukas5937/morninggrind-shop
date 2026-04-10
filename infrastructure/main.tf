de2da8483895f000fa6916ae4b7dadfb68c9fd0d
Lukas5937 <lukas-reich@t-online.de>
2026-04-10 11:37:27 +0200

    Implement terraform main.tf and create ec2 instances

---
+++ infrastructure/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_security_group" "ssh" {
+  name = "allow-ssh"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "staging" {
+  ami           = "ami-0a49b025fffbbdac6"
+  instance_type = "t3.micro"
+  key_name      = "morninggrind-key"
+
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  tags = {
+    Name = "morninggrind-staging"
+    Environment = "staging"
+    Project     = "morninggrind"
+  }
+}
+
+resource "aws_instance" "production" {
+  ami           = "ami-0a49b025fffbbdac6"
+  instance_type = "t3.micro"
+  key_name      = "morninggrind-key"
+
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+
+  tags = {
+    Name = "morninggrind-production"
+    Environment = "production"
+    Project     = "morninggrind"
+  }
+}
